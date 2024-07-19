be99b9fd552424b1940988bca13c473a55f44273
Igor Gabriel <91495485+Igor-Gabriel-D@users.noreply.github.com>
2024-07-19 20:22:36 -0300

    Update App.js

--- App.js
+++ App.js
@@ -14,12 +14,15 @@ const App = () => {
   const result = useState('');
 
   return (
-    
+      //   SafeAreaView para garantir que a UI esteja dentro da área segura do dispositivo.
+
       <SafeAreaView style={styles.container}>
         <View style={styles.resultContainer}>
           <Text style={styles.inputText}>{input}</Text>
           <Text style={styles.resultText}>{result}</Text>
         </View>
+
+        {/* Contêiner para os botões */}
         <View style={styles.buttonsContainer}>
           <TouchableOpacity
               key={'7'}
@@ -144,52 +147,48 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-
   container: {
-    flex: 1,
-    width: '100vw',
-    justifyContent: 'center',
-    maxWidth: 600,
-    margin: 'auto',
-
-    backgroundColor: '#9cc4e4'
+    flex: 1, // Ocupa todo o espaço disponível.
+    width: '100vw', // Define a largura como 100% da viewport.
+    justifyContent: 'center', 
+    maxWidth: 600, // Define uma largura máxima que o container com todos os botões e display pode ter.
+    margin: 'auto', // Centraliza horizontalmente para deixar mais responsivo em diferences dispositivos.
+    backgroundColor: '#9cc4e4' 
   },
   resultContainer: {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    padding: 20,
+    flex: 2, // Ocupa 2/7 do espaço disponível.
+    justifyContent: 'center', 
+    alignItems: 'flex-end', 
+    padding: 20, 
   },
   inputText: {
-    fontSize: height < 600 ? 36 : height * 0.06,
-    color: '#000',
+    fontSize: height < 600 ? 36 : height * 0.06, // Define o tamanho da fonte com base na altura da janela.
+    color: '#000', 
   },
   resultText: {
-    fontSize: height < 600 ? 48 : height * 0.07,
-    color: '#000',
+    fontSize: height < 600 ? 48 : height * 0.07, // Define o tamanho da fonte com base na altura da janela.
+    color: '#000', 
   },
   buttonsContainer: {
-    flex: 5,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'end',
-    borderTopWidth: 4,
-    borderTopColor: '#0fc3e8',
+    flex: 5, // Ocupa 5/7 do espaço disponível.
+    flexDirection: 'row', 
+    flexWrap: 'wrap', // Permite que os itens quebrem linha.
+    justifyContent: 'flex-end', 
+    borderTopWidth: 4, // Define a largura da borda superior.
+    borderTopColor: '#0fc3e8', // Define a cor da borda superior.
   },
   button: {
-    width: '25%',
-    height: '25%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    // backgroundColor: '#f0f0f0',
-    backgroundColor: '#487aa1'
+    width: '25%', // Define a largura dos botões para deixar em 4 colunas
+    height: '25%', // Define a altura dos botões para deixar em 4 linhas
+    justifyContent: 'center', // Centraliza o conteúdo verticalmente
+    alignItems: 'center', // Centraliza o conteúdo horizontalmente
   },
   buttonText: {
-    fontSize: height < 600 ? 24 : height * 0.04,
-    color: '#fff',
+    fontSize: height < 600 ? 24 : height * 0.04, // Define o tamanho da fonte com base na altura da janela.
+    color: '#fff', 
   },
   nonNumericButtonText: {
-    color: '#0fc3e8',
+    color: '#0fc3e8', 
   },
 });
 
